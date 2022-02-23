04ff3c46c0671295815503bfee58a8f5dc0c10e5
Julian Florez <forezjuli08@hotmail.com>
2022-02-22 22:53:40 -0500

    adding public files (subtree commit)

---
+++ js/scripts-min.js
@@ -0,0 +1,2 @@
+"use strict";for(var $shareBtns=document.querySelectorAll(".card__share-icon"),$shareContainer=document.querySelector("#share-container"),i=0;i<$shareBtns.length;i++)$shareBtns[i].addEventListener("click",(function(e){console.log(e.target,"click"),$shareContainer.classList.toggle("hidden")}));
+//# sourceMappingURL=../maps/scripts-min.js.map
